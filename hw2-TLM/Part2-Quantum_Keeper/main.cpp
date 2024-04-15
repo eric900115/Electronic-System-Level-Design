0718af47b4ee39966c3f058c4c25fc5afc917dc2
eric900115 <eric20607@gmail.com>
2024-04-15 15:06:56 +0800

    feat : add time profile in hw2-1 and hw2-2 (compare wait and time quantum)

--- hw2-TLM/Part2-Quantum_Keeper/main.cpp
+++ hw2-TLM/Part2-Quantum_Keeper/main.cpp
@@ -21,12 +21,21 @@ int sc_main(int argc, char **argv) {
          << endl;
     return 0;
   }
+
   Testbench tb("tb");
   GaussianFilter gaussian_filter("gaussian_filter");
   tb.initiator.i_skt(gaussian_filter.t_skt);
 
   tb.read_bmp(argv[1]);
+
+  gettimeofday(&start_time, NULL);
   sc_start();
+  gettimeofday(&end_time, NULL);
+
+  unsigned long execution_time =  1000000 * (end_time.tv_sec - start_time.tv_sec) + end_time.tv_usec - start_time.tv_usec;
+
+  std::cout << "Total Execution Time for the Program : " << execution_time << " microseconds" << std::endl;
+
   std::cout << "Simulated time == " << sc_core::sc_time_stamp() << std::endl;
   tb.write_bmp(argv[2]);
 
